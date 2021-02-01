efa2c18400e915320cb4e5e79522d5b700921cf0
JehyeonHeo <jhheo_heoje@naver.com>
2021-02-01 02:50:09 +0000

    Enter webapp address to verify prediction

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://boston-housing-prediction-flask.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
